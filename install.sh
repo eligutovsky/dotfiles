3aa8a328f258844ccbba9004ad1777e7093c6d33
Eli Gutovsky <github@gutovsky.me>
2022-10-11 16:42:41 +0300

    fix cancelation

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e pipefail
 HomebrewInstall() {
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
