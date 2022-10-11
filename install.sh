b3c5ddd822ff02b66a417d4552c783ff3261f060
Eli Gutovsky <github@gutovsky.me>
2022-10-11 16:28:55 +0300

    add install sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+HomebrewInstall() {
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+    echo "Installing Homebrew Casks..."
+    brew bundle;
+    echo "Homebrew and Casks installed"
+}
+
+ZshInstall() {
+  echo "Installing ZSH shell"
+  echo "Documentation: https://github.com/robbyrussell/oh-my-zsh"
+  echo "Installing plugins..."
+  git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
+  git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+  echo "Copying .zshrc file..."
+  cp .zshrc $HOME/.zshrc;
+}
+
+# SETUP
+echo "Setting up your Mac..."
+
+HomebrewInstall
+ZshInstall
+
+echo "Setup complete. Please restart your terminal"
